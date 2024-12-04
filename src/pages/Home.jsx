fd2b212ec19380b1e2dc2df40ff44b83d4a3227a
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-12-04 07:34:01 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,24 @@ import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex">
+     
       <Sidebar />
+
       
+      <div className="flex items-start p-4 gap-4 w-full">
+      
+        <div className="bg-blue-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
+
+     
+        <div className="bg-pink-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
+
+        
+        <div className="bg-orange-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
+
+        
+        <div className="bg-yellow-400 w-56 h-48 rounded-lg"></div>
+      </div>
     </div>
   );
 }
