e7b33b4d56610dd4e702e3fe480fa942611d73e0
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-12-04 10:56:02 -0300

    imagens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import Sidebar from "../components/Sidebar";
+import Graduation from "../assets/graduation.png"
+import Bookmark from "../assets/bookmark.png"
+import Usdsquare from "../assets/usd-square.png"
+import Vector from "../assets/vector.png"
 
 export default function Home() {
   return (
@@ -9,7 +13,9 @@ export default function Home() {
       
       <div className="flex items-start p-4 gap-4 w-full">
       
-        <div className="bg-blue-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
+        <div className="bg-blue-500 bg-opacity-30 w-56 h-48 rounded-lg">
+          <img src={Graduation} alt="graduation.pg" className="h-16" />
+        </div>
 
      
         <div className="bg-pink-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
