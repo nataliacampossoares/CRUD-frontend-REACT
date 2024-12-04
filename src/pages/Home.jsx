98093f6970a89b49f080faee89fb2a9471a3b786
laraheloisa <130068472+laraheloisa@users.noreply.github.com>
2024-12-04 11:12:29 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,18 +13,34 @@ export default function Home() {
       
       <div className="flex items-start p-4 gap-4 w-full">
       
-        <div className="bg-blue-500 bg-opacity-30 w-56 h-48 rounded-lg">
-          <img src={Graduation} alt="graduation.pg" className="h-16" />
+        <div className="bg-blue-500 bg-opacity-50 w-56 h-48 rounded-lg">
+          <img src={Graduation} alt="graduation.pg" className="h-16 ml-10" />
+          <p className="text-sm">Students</p>
+          <div className="flex justify-end items-end font-bold text-lg">243</div>
         </div>
 
      
-        <div className="bg-pink-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
+        <div className="bg-pink-500 bg-opacity-30 w-56 h-48 rounded-lg">
+          <img src={Bookmark} alt="bookmark.pg" className="h-16 ml-10" />
+          <p className="text-sm">Course</p>
+          <div className=" flez justify-end items-end font-bold text-lg">13</div>
+        </div>
 
         
-        <div className="bg-orange-500 bg-opacity-30 w-56 h-48 rounded-lg"></div>
+        <div className="bg-orange-500 bg-opacity-30 w-56 h-48 rounded-lg">
+          <img src={Vector} alt="vector.pg" className="h-16 ml-10" />
+          <p className="text-sm">Payments</p>
+          <div className="flex justify-end items-end font-bold text-sm">IRN</div>
+          <div className="flex justify-end items-end font-bold text-lg">243</div>
+          
+        </div>
 
         
-        <div className="bg-yellow-400 w-56 h-48 rounded-lg"></div>
+        <div className="bg-#FEAF00 w-56 h-48 rounded-lg">
+          <img src={Graduation} alt="graduation.pg" className="h-16 ml-10" />
+          <p className="text-sm">Users</p>
+          <div className="flex justify-end items-end font-bold text-lg">3</div> 
+        </div>
       </div>
     </div>
   );
