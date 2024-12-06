4af5fa4012e57c89bd29be0b9e8096e5730c1352
Lara Deitos <130068472+laraheloisa@users.noreply.github.com>
2024-12-06 15:50:34 -0300

    home/payment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import Vector from "../assets/vector.png";
 
 export default function Home() {
   return (
+    
     <div className="flex">
+      
       <Sidebar />
 
-      <div className="flex items-start p-4 gap-4 w-full">
-        <div className="relative bg-blue-500 bg-opacity-30 w-56 h-48 rounded-lg">
+      <div className="flex m-10 ml-auto p-4 gap-4">
+        <div className="relative bg-blue-500 bg-opacity-20 w-56 h-48 rounded-lg">
           <img
             src={Graduation}
             alt="graduation"
@@ -20,7 +22,7 @@ export default function Home() {
           <div className="absolute bottom-4 right-4 font-bold text-2xl">243</div>
         </div>
 
-        <div className="relative bg-pink-500 bg-opacity-30 w-56 h-48 rounded-lg">
+        <div className="relative bg-pink-500 bg-opacity-20 w-56 h-48 rounded-lg">
           <img
             src={Bookmark}
             alt="bookmark"
@@ -30,26 +32,27 @@ export default function Home() {
           <div className="absolute bottom-4 right-4 font-bold text-2xl">13</div>
         </div>
 
-        <div className="relative bg-orange-500 bg-opacity-30 w-56 h-48 rounded-lg">
+        <div className="relative bg-yellow-500 bg-opacity-20 w-56 h-48 rounded-lg">
           <img
-            src={Vector}
+            src={Usdsquare}
             alt="vector"
-            className="absolute top-4 left-4 w-12 h-12"
+            className="absolute top-4 left-4"
           />
           <p className="mt-16 ml-4 text-sm">Payments</p>
-          <div className="absolute bottom-4 right-4 text-right">
+          <div className="absolute bottom-4 right-4 flex items-center gap-2">
             <div className="font-bold text-sm">IRN</div>
             <div className="font-bold text-2xl">556,00</div>
           </div>
+
         </div>
 
-        <div className="relative bg-gradient-to-r from-yellow-400 to-yellow-600 bg-opacity-30 w-56 h-48 rounded-lg">
+        <div className="relative bg-gradient-to-r from-orange-400 to-yellow-400 bg-opacity-30 w-56 h-48 rounded-lg">
           <img
-            src={Usdsquare}
+            src={Vector}
             alt="users"
             className="absolute top-4 left-4"
           />
-          <p className="mt-16 ml-4 text-sm">Users</p>
+          <p className="mt-16 ml-4 text-white t">Users</p>
           <div className="absolute bottom-4 right-4 font-bold text-2xl">3</div>
         </div>
       </div>
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -5,19 +5,18 @@ export default function Payment(){
         <div className="flex">
           <Sidebar />
           <div className="ml-[280px] flex items-start p-4 gap-4 w-full relative">
-            <div className="flex flex-col w-full pr-20">
-            <div className="flex justify-between items-center m-5 w-full h-fit pb-4 pt-6 border-solid border-b-2 border-whiteGray">
-               <p className="font-bold text-xl">Paymentes Details</p>
-               
+          <div className="flex flex-col w-full pr-20">
+            <div className="flex justify-between items-center m-5 w-full h-fit pb-4  border-solid border-b-2 border-whiteGray">
+              <p className="font-bold text-xl">Payment Details</p>
             </div>
-            <div className="flex  justify-end">
-               <p className="text-lightGray2 font-semibold text-sm">Name</p>
-               <p className="text-lightGray2 font-semibold text-sm">Payment Schedule</p>
-               <p className="text-lightGray2 font-semibold text-sm">Bill Number</p>
-               <p className="text-lightGray2 font-semibold text-sm">Amount Paid</p>
-               <p className="text-lightGray2 font-semibold text-sm">Balance amount</p>
-               <p className="text-lightGray2 font-semibold text-sm">Day</p>
-            </div>
+            <div className="flex justify-between m-2 w-full h-fit p-2">
+              <p className="text-lightGray2 font-semibold text-xs w-[15%]">Name</p>
+              <p className="text-lightGray2 font-semibold text-xs w-[20%]">Payment Schedule</p>
+              <p className="text-lightGray2 font-semibold text-xs w-[15%]">Bill Number</p>
+              <p className="text-lightGray2 font-semibold text-xs w-[15%]">Amount Paid</p>
+              <p className="text-lightGray2 font-semibold text-xs w-[20%]">Balance Amount</p>
+              <p className="text-lightGray2 font-semibold text-xs w-[15%]">Day</p>
+             </div>
             </div>
           </div>
         </div>
